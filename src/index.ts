5495ac6c4944dba88ed8cedf85800dca7c5ae61e
Typed SIGTERM <typed.sigterm@gmail.com>
2024-07-22 19:39:03 +0800

    chore: add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import antfu from '@antfu/eslint-config';
 
+/**
+ * Construct an array of ESLint flat config items.
+ *
+ * @param options
+ *  The options for generating the ESLint configurations.
+ * @param userConfigs
+ *  The user configurations to be merged with the generated configurations.
+ * @returns
+ *  The merged ESLint configurations.
+ *
+ * @see https://github.com/antfu/eslint-config#customization
+ */
 const factory: typeof antfu = (options, ...userConfigs) => {
   return antfu(options, {
     name: 'typed-sigterm/stylistic/rules',
